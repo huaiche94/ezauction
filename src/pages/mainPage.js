d849f9cd43dc1c18ee53a5b93a6ce6ff63a7394d
shih-yao <sebasetian123@gmail.com>
2018-12-23 22:34:23 -0800

    integrate reactstrap : *reference : https://reactstrap.github.io

---
+++ src/pages/mainPage.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem
+} from 'reactstrap';
+class MainPage extends Component {
+	constructor(props) {
+		super(props);
+
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			isOpen: false
+		};
+	}
+	toggle() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+	render() {
+		return (
+			<div>
+				<Navbar color="light" light expand="md">
+					<NavbarBrand href="/">Market</NavbarBrand>
+					<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={this.state.isOpen} navbar>
+						<Nav className="ml-auto" navbar>
+							<NavItem>
+								<NavLink href="/components/">My Items</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink href="/bidding/">My Biddings</NavLink>
+							</NavItem>
+							<UncontrolledDropdown nav inNavbar>
+								<DropdownToggle nav caret>
+									Options
+                </DropdownToggle>
+								<DropdownMenu right>
+									<DropdownItem>
+										Option 1
+                  </DropdownItem>
+									<DropdownItem>
+										Option 2
+                  </DropdownItem>
+									<DropdownItem divider />
+									<DropdownItem>
+										Reset
+                  </DropdownItem>
+								</DropdownMenu>
+							</UncontrolledDropdown>
+						</Nav>
+					</Collapse>
+				</Navbar>
+			</div>
+		);
+	}
+}
+
+export default MainPage
