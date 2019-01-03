673a769a66d2e9f9d79beffa204b170bcb4f5b6d
shih-yao <sebasetian123@gmail.com>
2019-01-02 23:08:42 -0800

    firebase functions

---
+++ function/functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.push = functions.database.ref('/:id/name/')
+	.onCreate((snapshot, context) => {
+		// Grab the current value of what was written to the Realtime Database.
+		const original = snapshot.val();
+		console.log('Uppercasing', context.params.pushId, original);
+		const uppercase = original.toUpperCase();
+		// You must return a Promise when performing asynchronous tasks inside a Functions such as
+		// writing to the Firebase Realtime Database.
+		// Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+		return snapshot.ref.parent.child('uppercase').set(uppercase);
+});
